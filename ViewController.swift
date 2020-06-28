331e6bd8002617f752f23bd741e9db71fbe3a175
manpreetkaur-783 <66217528+manpreetkaur-783@users.noreply.github.com>
2020-06-27 22:11:44 -0400

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,14 +1,18 @@
 
-
 import UIKit
+import CoreData
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,subjectDataPass {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func passSubjectData(subjecttitle: String) {
+        print(subjecttitle)
+    }
 
 }
 
